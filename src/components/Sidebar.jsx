1c8b5b9d4dcabe554fd5b4659014fcb1aa4076f5
antonio <45393639+JoseA94@users.noreply.github.com>
2021-12-09 21:57:32 -0500

    Se sube el prototipo para el fron usando colores similares a los compartidos por el compañero Daniel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from 'context/authContext';
+import PrivateComponent from './PrivateComponent';
 
 const SidebarLinks = () => {
   return (
-    <ul className='mt-12'>
+    <ul className='mt-12 '>
       <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
+      <PrivateComponent roleList={['ADMINISTRADOR','LIDER']}>
+        <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-user' />
+      </PrivateComponent>
+      <SidebarRoute to='/proyectos' title='Proyectos' icon='fas fa-smile-wink' />
+      <PrivateComponent roleList={[ 'LIDER']}>
+        <SidebarRoute to='/inscripciones' title='Aprobacion Inscripciones' icon='fas fa-user' />
+      </PrivateComponent>
       <SidebarRoute to='/page2' title='Pagina2' icon='fas fa-smile-wink' />
       <SidebarRoute to='/category1' title='Catego 1' icon='fab fa-amazon' />
       <SidebarRoute to='/category1/page1' title='Test' icon='fas fa-car' />
+      <Logout />
     </ul>
+  );
+};
+
+const Logout = () => {
+  const { setToken } = useAuth();
+  const deleteToken = () => {
+    console.log('eliminar token');
+    setToken(null);
+  };
+  return (
+    <li onClick={() => deleteToken()}>
+      <NavLink to='/auth/login' className='sidebar-route text-pink-400'>
+        <div className='flex items-center'>
+          <i className='fas fa-sign-out-alt' />
+          <span className='text-sm  ml-2'>Cerrar Sesión</span>
+        </div>
+      </NavLink>
+    </li>
   );
 };
 
@@ -16,7 +44,7 @@ const Logo = () => {
   return (
     <div className='py-3 w-full flex flex-col items-center justify-center'>
       <img src='logo.png' alt='Logo' className='h-16' />
-      <span className='my-2 text-xl font-bold text-center'>Título de Mi Aplicación</span>
+      <span className='my-2 text-xl font-bold text-center'>Skill Project</span>
     </div>
   );
 };
@@ -24,11 +52,11 @@ const Logo = () => {
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
+    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full '>
       {/* Sidebar starts */}
 
-      <div className='sidebar hidden md:flex'>
-        <div className='px-8'>
+      <div className='sidebar hidden md:flex  '>
+        <div className='px-8 h-full bg-gray-600'>
           <Logo />
           <SidebarLinks />
         </div>
@@ -66,8 +94,8 @@ const SidebarRoute = ({ to, title, icon }) => {
         to={to}
         className={({ isActive }) =>
           isActive
-            ? 'sidebar-route text-white bg-indigo-700'
-            : 'sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400'
+            ? 'sidebar-route text-white bg-blue-600'
+            : 'sidebar-route text-white hover:text-white hover:bg-blue-400'
         }
       >
         <div className='flex items-center'>
