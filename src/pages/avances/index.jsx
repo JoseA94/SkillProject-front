e3e496a66b629cb0a8d17d03532c5c7327206476
Evelin Mendoza <90517661+EveMendo@users.noreply.github.com>
2021-12-17 21:57:15 -0500

    Se elimina botón de crear formulario
    Se elimina botón de crear formulario para colocarse en la interfaz de proyectos

--- src/pages/avances/index.jsx
+++ src/pages/avances/index.jsx
@@ -221,42 +221,6 @@ const IndexAvances = () => {
 };
 
 
-const CrearAvance = ({ proyecto, setOpenDialog }) => {
-  const { userData } = useUser();
-  const { form, formData, updateFormData } = useFormData();
 
-  const [crearAvance, { loading }] = useMutation(CREAR_AVANCE, {
-    refetchQueries: [GET_AVANCES],
-  });
-
-  const submitForm = (e) => {
-    e.preventDefault();
-
-    crearAvance({
-      variables: { ...formData, proyecto, creadoPor: userData._id },
-    })
-      .then(() => {
-        toast.success('avance creado con exito');
-        setOpenDialog(false);
-      })
-      .catch(() => {
-        toast.error('error creando el avance');
-      });
-  };
-  return (
-    <div className='p-4'>
-      <h1 className='text-2xl font-bold text-gray-900'>Crear Nuevo Avance {proyecto}</h1>
-      <form ref={form} onChange={updateFormData} onSubmit={submitForm}>
-        <Input name='descripcion' label='Descripción' type='text' />
-        <Input name='fecha' label='Fecha' type='date' />
-        <ButtonLoading
-          text='Crear Avance'
-          loading={loading}
-          disabled={Object.keys(formData).length === 0}
-        />
-      </form>
-    </div>
-  );
-};
 
 export default IndexAvances;
